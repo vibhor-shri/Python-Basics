e121ef6c2104c2ab7c8eb2412d3d563b8664e595
vibhor-shri <vibhor_shri@yahoo.co.in>
2020-02-20 18:00:31 +0530

    Arithmetic Operators explained String: Declaration & Assignment Basic operations on Strings

--- basics/ArithmeticOperators.py
+++ basics/ArithmeticOperators.py
@@ -1,2 +1,22 @@
 
 # Arithmetic operators in python
+seperator = "============================"
+print("Basic Arithmetic")
+print(seperator)
+
+# + Addition
+print(3+2)
+# - Subtraction
+print(3-2)
+# * MultiplicationS
+print(3*2)
+# /  fraction division
+print(3/2)
+# // Integer division
+print(3//2)
+# ** exponentiation
+print(3**2)
+# % remainder
+print(3%2)
+
+
---
+++ basics/DataTypes.py
@@ -0,0 +1,31 @@
+
+# Basic data types
+
+print ("String")
+print()
+my_string = 'Test'
+my_string1 = "Test"
+my_string2 = "Vibhor\'s String test"
+
+print (my_string)
+print (my_string1)
+print (my_string2)
+
+hello = "Hello"
+world = "World!"
+
+print()
+print (hello*5)
+print (world*5)
+
+print()
+print((hello+world + " ")*5)
+
+print()
+print(len(hello))
+print(len(world))
+print()
+
+first_element = hello[0]
+print(first_element)
+print(type(first_element))
