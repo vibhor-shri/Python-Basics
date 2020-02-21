15af9c69b9e72b43a92061bfde6b078eeefa059b
vibhor-shri <vibhor_shri@yahoo.co.in>
2020-02-22 00:00:02 +0530

    String Basics updated to explain partitioning in string

--- basics/StringBasics.py
+++ basics/StringBasics.py
@@ -29,3 +29,19 @@ print()
 first_element = hello[0]
 print(first_element)
 print(type(first_element))
+
+print()
+print("Partitioning in Strings")
+print("We can pick out a substring, or partition a string in the following way")
+my_new_string = "I love python. My love for java is decreasing"
+print("By calling my_new_string[0:12], we get the first 12 characters of my_new_string")
+print(my_new_string[0:13])
+
+print()
+print("Other examples of list partitioning")
+print("Get the elements in string starting from the 15th element, till the end of the string")
+print(my_new_string[15:])
+
+print()
+print("Get the elements in string starting from the 0th element, till the specified end index of the string")
+print(my_new_string[:15])
