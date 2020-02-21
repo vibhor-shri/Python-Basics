cc904103d4657aa3c8f6174780b9b836397daa8a
vibhor-shri <vibhor_shri@yahoo.co.in>
2020-02-22 00:02:31 +0530

    Code formatting updated for ArithmeticOperators.py

--- basics/ArithmeticOperators.py
+++ basics/ArithmeticOperators.py
@@ -7,21 +7,27 @@ print(separator)
 # + Addition
 print("Addition")
 print(3+2)
+
 # - Subtraction
 print("Subtraction")
 print(3-2)
+
 # * Multiplication
 print("Multiplication")
 print(3*2)
+
 # /  fraction division
 print("fraction division")
 print(3/2)
+
 # // Integer division
 print("Integer division")
 print(3//2)
+
 # ** exponentiation
 print("exponentiation")
 print(3**2)
+
 # % remainder
 print("remainder")
 print(3%2)
