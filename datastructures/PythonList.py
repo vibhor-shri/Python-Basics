b29923fcab00bb7f887e739a93cac9f5c2bad69b
vibhor-shri <vibhor_shri@yahoo.co.in>
2020-02-22 03:27:32 +0530

    PythonList completed

--- datastructures/PythonList.py
+++ datastructures/PythonList.py
@@ -16,7 +16,8 @@ print("List of same data type, eg given below")
 
 list_of_int_data_type = [3, 6, 8, 9, 1, 0]  # integer type list
 list_of_float_data_type = [3.14, 6.34, 8.67, 9.56, 1.12, 0.34]  # float type list
-list_of_string_data_type = ["a", "B", "c", "test", "anything in single or double quotes"]  # String type list
+list_of_string_data_type = ["a", "B", "c", "test", "anything in single or double quotes"]  # string type list
+list_of_languages = ["C", "C++", "C#", "Java", "Python", "Perl", "Kotlin"]  # another string type list
 list_of_bool_data_type = [True, False, False, True]  # boolean type list
 
 print("This is an int data type list: {}".format(list_of_int_data_type))
@@ -36,10 +37,7 @@ print("This is an bool data type list: {}".format(list_of_bool_data_type))
 print()
 print("Lists are indexed from 0 up to n-1, n being the length the list")
 print("first item in a list is at index 0, last item in a list is at index length of list -1")
-print("first item in float data type list = {}".format(list_of_float_data_type[0]))
-length = len(list_of_float_data_type)
-print("Length of the float data dype list = {}".format(length))
-print("last item in float data type list = {}".format(list_of_float_data_type[length-1]))
+
 print()
 print("To access 1st item in a list, first_item = list_name[0]")
 print("This is the first item in the int data type list: {}".format(list_of_int_data_type[0]))
@@ -79,9 +77,11 @@ print()
 print("Partitioning in list")
 print("List can be partitioned based on what we want from the list; eg below")
 list_of_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print()
+print("list of days in a week = {}".format(list_of_days))
 weekdays = list_of_days[0:5]
 weekends = list_of_days[5:7]
-print()
 print("Getting weekdays from the days list ----->  list_of_days[0:5] = {}".format(weekdays))
 print("Getting weekends from the days list -----> list_of_days[5:7] = {}".format(weekends))
 print("In the above example, notice how in [0:5], the start index, ie, 0 is inclusive and end index, ie, 5 is exclusive")
@@ -106,8 +106,111 @@ print(list_from_end_index_to_start_index)
 print("Notice, in each of the above samples, the order of the items are maintained.")
 
 print()
+print("Changing/Updating/Reassigning values in a list")
+print("We can change or update the value at a particular index in a list by simply re assigning a new item at "
+      "a particular index. Call can be made like, list_name[index value] = 'new item'")
+list_of_days[6] = 'Funday'
+print("Updated list of days = {}".format(list_of_days))
+print("The statement, list_of_days[6] = 'Funday' updated the value of the last item in the list")
+
+print()
+print("We can also append values to the end of the list by using the append method. Example below")
+print("In our list_of_days, we can add another item 'made up day', by calling the append method like: "
+      "list_of_days.append('made up day')")
+list_of_days.append("made up day")
+print(list_of_days)
+
+print()
 print("if an item is requested from a list, which is beyond the capacity of the list, then an error is returned."
       " Un comment the below code to see the error")
 # print(list_of_days[12])
+
+print()
+print("Few other methods in list")
+
+print()
+print("pop() method: Called on the list object.It removes the given item in the list, and return it. If optional "
+      "parameter [index] is not given, it pops the last item in the list")
+print("Testing of list.pop(). Called on the list_of_days object")
+
+print()
+print("List before pop is called = {}".format(list_of_days))
+item = list_of_days.pop()
+print("Item that was removed from the list = {}".format(item))
+print("List after pop is called = {}".format(list_of_days))
+
+print()
+print("List before pop is called = {}".format(list_of_days))
+item2 = list_of_days.pop(6)
+print("Item that was removed from the list, after calling pop(6) = {}".format(item2))
+print("List after pop is called with index value as 6 = {}".format(list_of_days))
+
+print()
+print("If pop() is called with an invalid index, an error is returned. Uncomment below code to test: "
+      "list_of_days.pop(22)")
+# item3 = list_of_days.pop(22)
+
+print()
+print("The max() method in a list returns the item with the maximum value in the list, or the last item "
+      "if the elements were sorted in ascending order")
+print("The max item in the int data type list list_of_int_data_type is = {}".format(max(list_of_int_data_type)))
+
+print()
+print("The max value in an float type list is the item with the maximum value in the list, or the last item "
+      "if the elements were sorted in ascending order")
+print("The max item in the float data type list list_of_float_data_type is = {}".format(max(list_of_float_data_type)))
+
+print()
+print("The max value in a string type list is the value which appears in the last, if the list is sorted alphabetically")
+print("The max item in the int data type list list_of_languages is = {}".format(max(list_of_languages)))
+
+print()
+print("Calling the max method in a list which has multiple data types, will result in error. Uncomment below line "
+      "to see the error")
+# max_item = max(list_of_multiple_data_type)
+
+print()
+print("The min() method in a list returns the item with the smallest value in the list, or the first item "
+      "if the elements were sorted in ascending order")
+print("The min item in the int data type list list_of_int_data_type is = {}".format(min(list_of_int_data_type)))
+
+print()
+print("The min value in an float type list is the item with the smallest value in the list, or the first item "
+      "if the elements were sorted in ascending order")
+print("The min item in the float data type list list_of_float_data_type is = {}".format(min(list_of_float_data_type)))
+
+print()
+print("The min value in a string type list is the value which appears in the first index, if the list is sorted alphabetically")
+print("The min item in the string data type list list_of_languages is = {}".format(min(list_of_languages)))
+
+print()
+print("Calling the min method in a list which has multiple data types, will result in error. Uncomment below line "
+      "to see the error")
+# min_item = min(list_of_multiple_data_type)
+
+print()
+print("The sorted() is called, taking a parameter as a list object, and it returns a copy of a list in order"
+      " from smallest to largest, leaving the original list unchanged.")
+print("Original list = {}".format(list_of_int_data_type))
+print("After calling sorted() method, list = {}".format(sorted(list_of_int_data_type)))
+
+print()
+print("The sorted() method can be called with an optional parameter, reverse=True, to return the same list in "
+      "descending order")
+print("Original list = {}".format(list_of_int_data_type))
+print("After calling sorted() method with reverse=True, list = {}".format(sorted(list_of_int_data_type, reverse=True)))
+
+print()
+print("Note: That calling sorted either ways, would not change the original list. It return only a copy of the "
+      "original list in sorted order, either ascending or descending")
+print("Checking the original list after sorted() calls: {}".format(list_of_int_data_type))
+
+print()
+print("Calling sorted on a list which has different data types will result in an error. Uncomment below lines to check")
+# print(sorted(list_of_multiple_data_type))
+
+print()
+print("len() method: Takes a parameter of list, and returns the size/length of the list")
+print("The length of list_of_days = {}".format(len(list_of_days)))
 
 
