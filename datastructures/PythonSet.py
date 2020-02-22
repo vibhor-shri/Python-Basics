9a83a4597332cb0ffdb56a4cc6526b82ddfccb11
vibhor-shri <vibhor_shri@yahoo.co.in>
2020-02-22 13:23:26 +0530

    PythonSet initial commit

---
+++ datastructures/PythonSet.py
@@ -0,0 +1,6 @@
+separator = "============================"
+print("Set in python")
+print(separator)
+print()
+
+print("A set is a data structure for mutable unordered collections of unique elements")
